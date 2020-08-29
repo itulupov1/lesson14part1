32a5e32125f0c716db841491244ea74d1db6c301
itulupov1 <itulupov1@yandex.ru>
2020-08-30 00:32:11 +0700

    fix(js)

--- js/script.js
+++ js/script.js
@@ -9,30 +9,26 @@ function DomElement(selector, height, width, bg, fontSize){
 	this.width = width;
 	this.bg = bg;
 	this.fontSize = fontSize;
-	this.newElem = function () {
-		const blockOne = document.createElement('div');
-		const blockTwo = document.createElement('p');
-		if (this.selector.substr(0, 1) === '.') {
-			blockOne.classList.add(this.selector.substr(1));
-			document.body.prepend(blockOne);
-			blockOne.style.cssText = 'height: '+this.height+'px; width: '+this.width+'px; background-color: '+this.bg+'; font-size: '+this.fontSize+'px;';
-			blockOne.innerHTML = 'Hello, world!';
-		}
-		if (this.selector.substr(0, 1) === '#') {
-			blockTwo.setAttribute('id', this.selector.substr(1));
-			document.body.prepend(blockTwo);
-			blockTwo.style.cssText = 'height: ' + this.height + 'px; width: ' + this.width + 'px; background-color: ' + this.bg + '; font-size: ' + this.fontSize + 'px;';
-			blockTwo.innerHTML = 'Hello, world2!';
-		} else {
-			return 0;
-		}
-	};
-	this.newElem(); // вызывается при создании нового объекта 
 }
+
+DomElement.prototype.newElem = function() {
+	let elem = document.createElement('div');
+	if (this.selector.substr(0, 1) === '.') {
+		elem.classList.add(this.selector.substr(1));
+	}
+	else if (this.selector.substr(0, 1) === '#') {
+		elem = document.createElement('p');
+		elem.setAttribute('id', this.selector.substr(1));
+	} else {
+		return 0;
+	}
+	elem.style.cssText = `height: ${this.height}px; width: ${this.width}px; background-color: ${this.bg}; font-size: ${this.fontSize}px;`;
+	elem.textContent = 'Hello, world!';
+	document.body.prepend(elem);
+};
 
 const newDomElem = new DomElement(newClass, 50, 100, 'red', 21);
 const newElemToo = new DomElement(newId, 100, 150, 'green', 32);
 
-//newDomElem.newElem(); вызов метода вручную
-//newElemToo.newElem(); вызов метода вручную
-
+newDomElem.newElem(); 
+newElemToo.newElem();
